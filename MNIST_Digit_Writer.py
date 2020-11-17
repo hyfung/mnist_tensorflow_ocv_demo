ac0de1c78828d9f975c9860bf84c4dc59f3f02b8
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-17 19:43:37 +0800

    Yay, freaking done

--- MNIST_Digit_Writer.py
+++ MNIST_Digit_Writer.py
@@ -1,34 +1,45 @@
+'''
+https://www.tensorflow.org/tutorials/quickstart/beginner
+'''
+import tensorflow as tf
+
 import numpy as np
 import cv2
-
-
-import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10),
+    ])
+
+predictions = model(x_train[:1]).numpy()
+
+tf.nn.softmax(predictions).numpy()
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
+loss_fn(y_train[:1], predictions).numpy()
+
 model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+    loss=loss_fn,
+    metrics=['accuracy'],
+    )
 
-model.fit(x_train, y_train, epochs=1    )
-    
+model.fit(x_train, y_train, epochs=1)
+
+# model.evaluate(x_test,  y_test, verbose=2)
+
 probability_model = tf.keras.Sequential([
-  model,
-  tf.keras.layers.Softmax()
-])
+    model,
+    tf.keras.layers.Softmax()
+    ])
 
+#--------------------------------------------------------------#
 
 draw = False
 mat = np.zeros((28,28), dtype=np.uint8)
@@ -45,11 +56,11 @@ def mouse_cb(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         draw = False
         print('stop drawing')
-        print(mat)
+        # print(mat)
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if draw:
-            print(x,y)
+            # print(x,y)
             mat[y][x] = 255
 
     elif event == cv2.EVENT_LBUTTONDBLCLK:
@@ -62,9 +73,9 @@ cv2.setMouseCallback("Drawboard", mouse_cb)
 
 while True:
     cv2.imshow('Drawboard', mat)
-    normalized_mat = mat / 255.0
 
-    print(probability_model(normalized_mat))
-
+    new_mat = np.reshape(mat,(1,784))
+    print(np.argmax(probability_model(new_mat)))
+    
     if cv2.waitKey(100) & 0xFF == ord('q'):
         break
